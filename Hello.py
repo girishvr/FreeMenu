455087cd6df4b61230c7ef135c5672dd9d39df6f
Girish <girishvinr@gmail.com>
2023-08-02 22:42:12 +0530

    Project Set up for FreeMenu

---
+++ Hello.py
@@ -0,0 +1,12 @@
+#Hello.py
+#First file for Flask project
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+if __name__ == '__main__':
+	app.run()	
