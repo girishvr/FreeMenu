1f73e44133ee6f7452beb5ebcae7d6f1a5a8273b
Girish <girishvinr@gmail.com>
2023-08-03 23:05:17 +0530

    CSS added, structure reorganised

--- Start.py
+++ Start.py
@@ -1,12 +1,14 @@
 #Start.py
 #First file for Flask project
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def index():
+	dict = {'Tea':50,'Coffee':60,'Toast':70}
+	return render_template("index.html", name = "Darling", result = dict)
 
 # The route() decorator in Flask is used to bind URL to a function
 @app.route('/hello')
