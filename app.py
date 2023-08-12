6574b539385dd3e5448329d20b374a49bbd93873
Girish <girishvinr@gmail.com>
2023-08-12 22:52:58 +0530

    app file updated for production

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ def helloworld():
 
 
 #START of the program
-if __name__ == '__main__':
-    app.debug = True
-    app.run()   
+# if __name__ == '__main__':
+    # app.debug = True
+    # app.run()   
     # app.run(debug = True)
