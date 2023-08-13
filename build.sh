c494b1dc43ee33d8cdfec1e528a29a59c9b881c7
Girish <girishvinr@gmail.com>
2023-08-13 18:33:39 +0530

    index page bug and build script

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
-source venv/bin/activate
-python3 app.py
+#! /bin/bash
+
+#source venv/bin/activate
+#python3 app.py
+
+echo "Updating the files..."
+git pull origin master
+
+echo "Restarting the server..."
+sudo service apache2 restart
+
+echo "Recent logs:"
+
+sudo tail -50 /var/log/apache2/error-5000.log
+sudo tail -50 /var/log/apache2/access-5000.log
+
+echo "Build complete."
